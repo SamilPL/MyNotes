d01fedbd8579a7b58e8e74e043aa20cab17bf787
SamilPL <samil850109@gmail.com>
2021-03-22 11:26:11 +1000

    Added Angular material

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  title = 'My Notes App';
+
+  constructor (private http: HttpClient) {}
+
+  ngOnInit() {
+    throw new Error('Method not implemented.');
+  }
+
+  formatLabel(value: number) {
+    if (value >= 1000) {
+      return Math.round(value / 1000) + 'k';
+    }
+
+    return value;
+  }
+}
